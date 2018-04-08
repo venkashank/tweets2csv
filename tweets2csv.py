c0162ff4741db448068341948c964cc3b9e8df69
sqrt64 <>
2018-04-08 17:15:21 -0400

    Updates

--- tweets2csv.py
+++ tweets2csv.py
@@ -3,19 +3,20 @@ import csv
 import json
 import tweepy
 
+
 def read_credentials():
     """
     Fill out credentials.json.example and save as credentials.json. 
     You must create API credentials from Twitter directly before running this script.
     """
-    
+
     try:
         with open("credentials.json", "r") as f:
             credentials = json.load(f)
     except FileNotFoundError:
         print("Credentials.json not found")
         raise
-    return credentials 
+    return credentials
 
 
 def twitter_auth(credentials):
@@ -47,7 +48,7 @@ def search_query(retweets):
     """
     Builds the search query by checking if we want to include retweets.
     """
-    if retweets  == False:
+    if not retweets:
         search_query = "-filter:retweets"
     else:
         search_query = ""
@@ -60,7 +61,7 @@ def twitter_search(query, lang, items):
     Returns a Tweepy Cursor object
     """
     return tweepy.Cursor(t.search, q=query, lang=lang).items(items)
-    
+
 
 def csv_write(tweet, filename):
     """
@@ -84,13 +85,12 @@ def tweet_parse(tweet):
     tweet_text = tweet_text.replace('\n', ' ')
     hashtags = []
     for i in tweet.entities['hashtags']:
-            hashtags.append(i['text'])
+        hashtags.append(i['text'])
     favorited = tweet.favorited
     favorite_count = tweet.favorite_count
     retweeted = tweet.retweeted
     retweet_count = tweet.retweet_count
     tweeted_from = tweet.source
-
 
     parsed_tweet = [
         tweet_date,
@@ -109,54 +109,54 @@ def tweet_parse(tweet):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-                        add_help=True,
-                        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-                        description="Select Options",
-                        prefix_chars='-')
+        add_help=True,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Select Options",
+        prefix_chars='-')
 
     parser.add_argument(
-                        '-q', '--query',
-                        help='Your Twitter Search Query',
-                        action='store',
-                        dest='query',
-                        required=True)
+        '-q', '--query',
+        help='Your Twitter Search Query',
+        action='store',
+        dest='query',
+        required=True)
 
     parser.add_argument(
-                        '-o', '--output_file',
-                        help='Output File Name (.csv)',
-                        action='store',
-                        dest='output_file',
-                        default='tweets.csv',
-                        required=False)
+        '-o', '--output_file',
+        help='Output File Name (.csv)',
+        action='store',
+        dest='output_file',
+        default='tweets.csv',
+        required=False)
 
     parser.add_argument(
-                        '-n', '--number',
-                        help='Number of Tweets (Default = 10)',
-                        required=True,
-                        default=10,
-                        action='store',
-                        dest='number_of_tweets',
-                        type=int)
+        '-n', '--number',
+        help='Number of Tweets (Default = 10)',
+        required=True,
+        default=10,
+        action='store',
+        dest='number_of_tweets',
+        type=int)
 
     parser.add_argument(
-                        '-rt', '--retweets',
-                        help='Include ReTweets? Default = False',
-                        action='store',
-                        dest='retweets',
-                        default=False)
+        '-rt', '--retweets',
+        help='Include ReTweets? Default = False',
+        action='store',
+        dest='retweets',
+        default=False)
 
     parser.add_argument(
-                        '-l', '--language',
-                        help='Language to search. Default = "en"',
-                        action='store',
-                        dest='tweet_language',
-                        default="en")
+        '-l', '--language',
+        help='Language to search. Default = "en"',
+        action='store',
+        dest='tweet_language',
+        default="en")
 
     args = parser.parse_args()
-    
-    t =  twitter_auth(read_credentials())
+
+    t = twitter_auth(read_credentials())
     search_query = search_query(args.retweets)
 
     search = twitter_search(search_query, args.tweet_language, args.number_of_tweets)
     for tweet in search:
-            csv_write(tweet_parse(tweet), args.output_file)
+        csv_write(tweet_parse(tweet), args.output_file)
